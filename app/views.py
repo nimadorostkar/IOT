dd7c33f5a490107212f345021238dec4c00a5ce0
nimadorostkar <nimadorostkar97@gmail.com>
2021-07-31 12:19:30 +0430

    profile form and view

--- app/views.py
+++ app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
-
+from . import models
+from .models import Profile
+from .forms import ProfileForm, UserForm
 
 
 
@@ -17,13 +19,37 @@ def index(request):
     return HttpResponse(html_template.render(context, request))
 
 
- 
+
 
 
 @login_required(login_url="/login/")
 def profile(request):
+    profile = models.Profile.objects.filter(user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            username = user_form.cleaned_data['username']
+            first_name = user_form.cleaned_data['first_name']
+            last_name = user_form.cleaned_data['last_name']
+            email = user_form.cleaned_data['email']
+            password1 = user_form.cleaned_data['password1']
+            password2 = user_form.cleaned_data['password2']
+            phone = profile_form.cleaned_data['phone']
+            address = profile_form.cleaned_data['address']
+            user_photo = profile_form.cleaned_data['user_photo']
+            user_form.save()
+            profile_form.save()
+            context = {'user_form':user_form, 'profile_form':profile_form}
+            context['segment'] = 'profile'
 
-    context = {}
+            html_template = loader.get_template( 'accounts/profile.html' )
+            return HttpResponse(html_template.render(context, request))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+
+    context = {'user_form':user_form, 'profile_form':profile_form}
     context['segment'] = 'profile'
 
     html_template = loader.get_template( 'accounts/profile.html' )
